054321a899ef365addf3de767bde27f24d1eba5f
vikas <vicky582050@gmail.com>
2024-02-02 16:33:04 +0530

    phase1 commit

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const dbConn = require("./init/db");
 const Listing = require("./models/listing.js");
 const path = require("path");
+const over_ride = require("method-override");
 
 dotenv.config();
 
@@ -15,7 +16,8 @@ let PORT = process.env.PORT;
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
-app.use(express.urlencoded({extended: true}));     //data coming in req get parsed
+app.use(express.urlencoded({ extended: true })); //data coming in req get parsed
+app.use(over_ride("_method"))
 
 app.get("/", (req, res) => {
   res.send("fine looking site");
@@ -23,15 +25,41 @@ app.get("/", (req, res) => {
 
 app.get("/list", async (req, res) => {
   const houseList = await Listing.find({});
-  res.render("listinghouse/index.ejs",{houseList});
+  res.render("listinghouse/index.ejs", { houseList });
 });
 
-app.get("/list/:id",async (req,res)=>{
-    let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render('./listinghouse/show.ejs',{listing})
+//lists route
+app.get("/list/new", (req, res) => {
+  res.render("listinghouse/newForm.ejs");
+});
+
+//create Route
+app.post("/list", async (req, res) => {
+  const newLists = new Listing(req.body.lists);
+  await newLists.save();
+  res.redirect("/list");
+});
+
+app.get("/list/:id", async (req, res) => {
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listinghouse/show.ejs", { listing });
+});
+
+//edit Route
+app.get("/list/:id/edit",async (req,res)=>{
+  let { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("listinghouse/edit.ejs",{listing})
 })
 
+//update Route
+app.put("/list/:id",async (req,res)=>{
+  let { id } = req.params;
+  await Listing.findByIdAndUpdate(id,{...req.body.listing});
+  res.redirect("/list");
+})
+ 
 app.listen(PORT, () => {
   console.log(`listening this at ${LOCALHOST}${PORT}`);
 });
